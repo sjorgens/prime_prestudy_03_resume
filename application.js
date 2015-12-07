ed476c1dfb9087b061b366c60a1a1f1971f12c84
sjorgens <scottjorgens@netscape.net>
2015-12-06 22:20:43 -0600

    Initial Commit

---
+++ application.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	// runs when the DOM is ready
+
+	// show the HTML document on button click
+	$('button').on('click', function() {
+		$('body').css('visibility', 'visible');
+	});
+});
